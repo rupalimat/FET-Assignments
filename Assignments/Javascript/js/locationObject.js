8486599935c542dda621f4659049c5b73dc631fe
Rupali <rupalimat@cybage.com>
2022-02-08 13:36:23 +0530

    Working on assignments

---
+++ Assignments/Javascript/js/locationObject.js
@@ -0,0 +1,23 @@
+// Prints complete URL
+        document.write("URL IS:  " 
+                + window.location.href + "<br>");
+  
+        // Prints hostname like local host (www.example.com)
+        document.write("HOSTNAME:  " 
+                + window.location.hostname + "<br>");
+  
+        // Prints pathname like /products/find.php
+        document.write("PATHNAME:  " 
+                + window.location.pathname + "<br>");
+  
+        // Prints the protocol used like http: or https:
+        document.write("PROTOCOL:  " 
+                + window.location.protocol + "<br>");
+  
+        // Prints hostname with port like localhost:3000
+        document.write("HOSTNAME WITH PORT:  " 
+                + window.location.host + "<br>");
+  
+        // Prints port number like 3000
+        document.write("PORTNUMBER:  " 
+                + window.location.port + "<br>");
